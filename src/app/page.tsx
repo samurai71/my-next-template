937148320c9b9288481d687a9dab54f2dcfbbad0
Mark Landeryou <samurai96@hotmail.com>
2025-10-04 20:04:16 -0400

    refactor: standardize code formatting and improve consistency across components
    - Updated import statements to use consistent semicolon placement.
    - Refactored various components to ensure consistent return statements.
    - Enhanced readability by adding missing semicolons and formatting adjustments.
    - Added husky pre-commit hook for lint-staged to enforce code quality.
    - Introduced .prettierignore to exclude build and coverage artifacts from formatting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toogle";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-       <ModeToggle />
+      <ModeToggle />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-       
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -54,8 +53,8 @@ export default function Home() {
             Read our docs
           </a>
           <div>
-      <Button>Click me</Button>
-    </div>
+            <Button>Click me</Button>
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
