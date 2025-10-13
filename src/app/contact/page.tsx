7005cc8a694642ffea856b01919e5a648bb5355d
Mark Landeryou <samurai96@hotmail.com>
2025-10-12 21:34:33 -0400

    feat: :sparkles: added a shadcn form to contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,14 @@
+import MyForm from '@/components/contactform'
 import { ModeToggle } from '@/components/mode-toogle'
 
-export default function Home() {
+export default function Contact() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <ModeToggle />
-      <h1>Contact Us</h1>
+      <main>
+        <h1 className="font-serif text-5xl font-bold">Contact Us</h1>
+        <MyForm />
+      </main>
     </div>
   )
 }
