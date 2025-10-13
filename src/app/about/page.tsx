99a843485a5e51a0cfe1f9ce347f6d0af386e996
Mark Landeryou <samurai96@hotmail.com>
2025-10-12 21:46:51 -0400

    style: :lipstick: added some classes to styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <ModeToggle />
-      <h1>About Us Page</h1>
+      <main>
+        <h1>About Us Page</h1>
+      </main>
     </div>
   )
 }
