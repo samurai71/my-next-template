ead230ba24e5528ed33f284493c4d9d12d076f31
Mark Landeryou <samurai96@hotmail.com>
2025-10-12 23:48:40 -0400

    style: :lipstick: fixed the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,46 @@ export default function About() {
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <ModeToggle />
       <main className="container mx-auto w-5xl px-3 py-3">
-        <h1>About Us Page</h1>
+        <h1 className="pb-3">About Us</h1>
+        <p className="pb-3">
+          Nisi consequat irure laboris fugiat. Qui ad culpa ad sunt esse
+          pariatur commodo ipsum laboris proident et reprehenderit. Non pariatur
+          velit in excepteur adipisicing mollit pariatur deserunt officia ut.
+          Cillum incididunt laboris proident aute ad dolore. Sint nisi officia
+          reprehenderit incididunt. Ut sunt labore laborum pariatur fugiat enim
+          officia nulla cillum minim. Consequat pariatur adipisicing eiusmod
+          cillum magna fugiat proident sunt enim esse minim esse ut.
+        </p>
+
+        <p className="pb-3">
+          Nisi consequat irure laboris fugiat. Qui ad culpa ad sunt esse
+          pariatur commodo ipsum laboris proident et reprehenderit. Non pariatur
+          velit in excepteur adipisicing mollit pariatur deserunt officia ut.
+          Cillum incididunt laboris proident aute ad dolore. Sint nisi officia
+          reprehenderit incididunt. Ut sunt labore laborum pariatur fugiat enim
+          officia nulla cillum minim. Consequat pariatur adipisicing eiusmod
+          cillum magna fugiat proident sunt enim esse minim esse ut.
+        </p>
+
+        <p className="pb-3">
+          Nisi consequat irure laboris fugiat. Qui ad culpa ad sunt esse
+          pariatur commodo ipsum laboris proident et reprehenderit. Non pariatur
+          velit in excepteur adipisicing mollit pariatur deserunt officia ut.
+          Cillum incididunt laboris proident aute ad dolore. Sint nisi officia
+          reprehenderit incididunt. Ut sunt labore laborum pariatur fugiat enim
+          officia nulla cillum minim. Consequat pariatur adipisicing eiusmod
+          cillum magna fugiat proident sunt enim esse minim esse ut.
+        </p>
+
+        <p className="pb-3">
+          Nisi consequat irure laboris fugiat. Qui ad culpa ad sunt esse
+          pariatur commodo ipsum laboris proident et reprehenderit. Non pariatur
+          velit in excepteur adipisicing mollit pariatur deserunt officia ut.
+          Cillum incididunt laboris proident aute ad dolore. Sint nisi officia
+          reprehenderit incididunt. Ut sunt labore laborum pariatur fugiat enim
+          officia nulla cillum minim. Consequat pariatur adipisicing eiusmod
+          cillum magna fugiat proident sunt enim esse minim esse ut.
+        </p>
       </main>
     </div>
   )
