a796a7e2bcd1961bc17151b89030a343fb3d93d0
Mark Landeryou <samurai96@hotmail.com>
2025-10-15 19:19:48 -0400

    fix: made the two links buttons in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <ModeToggle />
-      <main className="container mx-auto px-3 py-3 sm:w-3xs lg:w-5xl"></main>
+      <main className="container mx-auto px-3 py-3 sm:w-3xs lg:w-5xl">
+        <h1>Home Page</h1>
+      </main>
     </div>
   )
 }
