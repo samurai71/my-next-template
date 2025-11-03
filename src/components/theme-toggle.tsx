7d02f1e43530dec0c1e3f4d45c2f091c4ef01714
Mark Landeryou <samurai96@hotmail.com>
2025-11-03 12:58:15 -0500

    Develop (#2)
    * added next-themes
    
    * added husky prettier lint-staging
    
    * ran prettier
    
    * made some changes
    
    * ran prettier
    
    * added two words to the settings
    
    * added a word to the settings dot json
    
    * updated packages in package dot json
    
    * wip trying to add dark and light mode
    
    * made some changes
    
    * refactor: simplify ThemeProviderProps by using NextThemesProvider's props
    
    * feat: :sparkles: added commitlint
    
    * docs: :memo: created a todo md for myself

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import * as React from 'react'
+
+import { useTheme } from 'next-themes'
+
+import { Moon, Sun } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  // Prevent hydration mismatch
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+      <Button variant="outline" size="icon">
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    )
+  }
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'dark' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
