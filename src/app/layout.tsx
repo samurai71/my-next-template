4d65b636a9c2ea5515fdba2a6b63fcf59c4e8342
Mark Landeryou <samurai96@hotmail.com>
2025-11-10 15:55:45 -0500

    feat: :sparkles: tried to add a footer component to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 
+// import { Footer } from '@/components/Footer'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import '@/styles/globals.css'
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        {/* <Footer /> */}
       </body>
     </html>
   )
